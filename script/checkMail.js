edcd9baed8f40e3029d37e4f569d88ff50e00951
ThiCode126 <59418646+ThiCode126@users.noreply.github.com>
2021-01-02 19:04:27 +0100

    Error Fix

--- script/checkMail.js
+++ script/checkMail.js
@@ -1,19 +1,13 @@
 
 document.getElementById('form-mail').addEventListener('submit', function(e) {
     e.preventDefault();
-    let errorSpan = document.getElementById('error-msg').value;
-
-    console.log(errorSpan)
-
+    let errorSpan = document.getElementById('error-msg');
     const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const mail = e.target[0].value;
 
-    errorSpan = "";
+    errorSpan.innerHTML = "";
 
-    if(mail.match(mailFormat)) {
-        console.log(mail + " : adresse valide !");
-    } else {
-        console.log(mail + " : adresse INvalide !");
-        errorSpan = "Oops! Please check your email";
+    if(!mail.match(mailFormat)) {
+        errorSpan.innerHTML = "Oops! Please check your email";
     }   
 })
